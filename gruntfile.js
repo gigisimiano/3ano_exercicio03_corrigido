6e1f5382641a8108c1aaf990ec85577df84da263
gigisimiano <giovana.0936@aluno.pr.senac.br>
2025-04-07 12:27:22 -0300

    07/04/2025 - 12:26h

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,26 @@
 module.exports = function(grunt){
     grunt.initConfig({
         pkg: grunt.file.readJson('package.json'),
+        sass:{
+            dsit:{
+                options:{
+                    style: 'compressed'
+                },
+                files:{
+                'dist/styles/main.css': 'src/styles/main.css'
+            }
+            }
+        },
+        watch:{
+            sass:{
+                files: "src/styles/**/*.scss",
+                tasks: ['sass']
+            }
+        }
     })
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch')
+
+    grunt.registerTask('default', ['sass', 'watch']);
 }
